e6588d8b8a32ff0f819b054879063d92982e2cfd
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2025-07-22 09:47:28 +0700

    Create styleModes.js

---
+++ constants/styleModes.js
@@ -0,0 +1,17 @@
+/**
+ * List of supported text styling modes.
+ *
+ * Modes:
+ * - 'line': Apply style to the entire string.
+ * - 'sentence': Apply style per sentence.
+ * - 'word': Apply style per word.
+ * - 'char': Apply style per character.
+ */
+const styleModes = [
+  'line',
+  'sentence',
+  'word',
+  'char'
+];
+
+export default styleModes;
