fbf6c2ee8ade3394f1548582a4e84ec8c0171009
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2025-07-22 09:50:26 +0700

    Create brightBgColorCodes.js

---
+++ constants/brightBgColorCodes.js
@@ -0,0 +1,17 @@
+/**
+ * ANSI background color codes for bright terminal colors.
+ *
+ * Maps bright background color names to their corresponding ANSI codes (100–107).
+ */
+const brightBgColorCodes = {
+  bgBrightBlack: '100',
+  bgBrightRed: '101',
+  bgBrightGreen: '102',
+  bgBrightYellow: '103',
+  bgBrightBlue: '104',
+  bgBrightMagenta: '105',
+  bgBrightCyan: '106',
+  bgBrightWhite: '107'
+};
+
+export default brightBgColorCodes;
