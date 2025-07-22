96aecd69f65643e6c16e4ff8a2d54646d9238e76
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2025-07-22 09:48:50 +0700

    Create fgColorCodes.js

---
+++ constants/fgColorCodes.js
@@ -0,0 +1,17 @@
+/**
+ * ANSI foreground color codes for standard terminal colors.
+ *
+ * Maps color names to their respective ANSI escape code values (30–37).
+ */
+const fgColorCodes = {
+  black: '30',
+  red: '31',
+  green: '32',
+  yellow: '33',
+  blue: '34',
+  magenta: '35',
+  cyan: '36',
+  white: '37'
+};
+
+export default fgColorCodes;
